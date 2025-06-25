725cc4b6759953f265f25d41790fac2640b1fd2a
felixonwheels <felixgif@gmail.com>
2025-06-25 19:42:27 +0200

    changes name from wellwellwell to robinet

--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -1,7 +1,7 @@
 <h2
 	class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
 >
-	Well well well
+	🚰 robinet
 </h2>
 
 <p class="text-muted-foreground text-xl">
